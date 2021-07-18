abfc01bd2cf59eeb93285f6e1390cddcb016077e
vivek sharma <vivekslim@gmail.com>
2021-07-18 15:44:20 +0530

    complete assignment II

--- block-BNaabw/sample/server.js
+++ block-BNaabw/sample/server.js
@@ -1,26 +1,37 @@
 var express = require("express")
-var app = express()
+var app = express() // instantiate the app by mounting
 var logger = require("morgan")
 var cookieParser  = require("cookie-parser")
 
 // built-in middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(express.static(__dirname+ "/index.html"))
+app.use(express.static(__dirname+ "/public"))
+
+//3rd part middlewares
+app.use(logger("dev"))
+app.use(cookieParser())
 
 //custom middleware
 app.use("/admin" , (req,res,next)=> {
     next("unauthorized to access")   // when next called ,it will skip all routes and middleware and will go to line number 33
+                                       // next function without argument simply go from one middleware to another middleware
 })
 
 //route handling
 app.get("/" , (req,res)=> {
-    res.send("Testing")
+    res.sendFile(__dirname+ "/index.html")
 })
 
 app.get("/users" , (req,res)=> {
-    res.sendFile(__dirname+ "/index.html")
+    res.send("USERS")
 })
+
+app.get("/project" , (req,res)=> {
+    res.sendFile(__dirname+"/project.html")
+})
+
+
 
 //Error handling middlewares
 
@@ -31,9 +42,11 @@ app.use((req,res,next)=> {
 //custom error handle
 
 app.use((err,req,res,next)=> {
-    res.status(500).send(err)
+    res.status(500).send(err)   
 })
 
+
+//listener
 app.listen(5000, ()=> {
     console.log("server listening to port 5000")
 })
